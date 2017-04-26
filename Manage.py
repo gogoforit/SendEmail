3541bb0c3ab7f62b31d1aada5028144d9a5d3b0c
gogoforit <715157026@qq.com>
2017-04-26 22:12:24 +0800

    成功完成文件的转移，压缩，和发送邮件

--- Manage.py
+++ Manage.py
@@ -1,8 +1,27 @@
 import os
-os.chdir('/')
-os.system('sudo mv 2017-04-22 ~/SignInfo')
-os.chdir('/home/pi/SignInfo')
-os.system('sudo zip -r 2017-04-22.zip 2017-04-22')
+import SendTest_File
+import re
 
+
+os.chdir('/')
+file_name = '2017-04-13'
+
+isExists = os.path.exists(file_name)
+
+
+if isExists:
+	os.chdir('/')
+	command_mv_file = 'sudo mv ' + file_name + ' ~/SignInfo'
+	os.system(command_mv_file)
+	command_cd_host = '/home/pi/SignInfo'
+	os.chdir(command_cd_host)
+	command_zip_file = 'sudo zip -r ' + file_name + '.zip' + ' ' + file_name
+	os.system(command_zip_file)
+	the_file_path = command_cd_host + '/' + file_name + '.zip'
+	the_file_name = file_name + '.zip'
+	SendTest_File.send_email(the_file_path,the_file_name,file_name)	
+	
+else:
+	print('文件已转移！')
 
 
--- SendTest_File.py
+++ SendTest_File.py
@@ -6,40 +6,45 @@ from email.mime.text import MIMEText
 from email.mime.multipart import MIMEMultipart
 from email.header import Header
 
+def send_email(file_path,file_name,the_time):
+	my_sender='wangxinyuan_sign@aliyun.com' #发件人邮箱账号，为了后面易于维护，所以写成了变量
+	my_user='715157026@qq.com' #收件人邮箱账号，为了后面易于维护，所以写成了变量
+	#file_path = ''
+	#file_name = ''
+	title_name = '每日签到文件' + the_time 
 
-my_sender='wangxinyuan_sign@aliyun.com' #发件人邮箱账号，为了后面易于维护，所以写成了变量
-my_user='715157026@qq.com' #收件人邮箱账号，为了后面易于维护，所以写成了变量
-file_path = ''
-file_name = ''
+	# 创建一个带附件的实例
+	message = MIMEMultipart()
+	message['From'] = Header("网信院签到", 'utf-8')
+	message['To'] = Header("适闲", 'utf-8')
+	subject = title_name
+	message['Subject'] = Header(subject, 'utf-8')
 
-# 创建一个带附件的实例
-message = MIMEMultipart()
-message['From'] = Header("2017-04-24", 'utf-8')
-message['To'] = Header("适闲", 'utf-8')
-subject = '每日签到文件-04-25-2017'
-message['Subject'] = Header(subject, 'utf-8')
+	# 邮件正文内容
+	message.attach(MIMEText('今天的签到情况,见附件', 'plain', 'utf-8'))
 
-# 邮件正文内容
-message.attach(MIMEText('今天的签到情况', 'plain', 'utf-8'))
+	# 构造附件1，传送当前目录下的 test.txt 文件
+	att1 = MIMEText(open(file_path, 'rb').read(), 'base64', 'utf-8')
+	att1["Content-Type"] = 'application/octet-stream'
+	# 这里的filename可以任意写，写什么名字，邮件中显示什么名字
+	#构造发送的文件名字
+	the_command = 'attachment; filename=' + '"'+ file_name +'"' + "'"
+	print(the_command)
+	att1["Content-Disposition"] = the_command
+	#att1["Content-Disposition"] = 'attachment; filename="2017-04-05.zip"'
+	message.attach(att1)
 
-# 构造附件1，传送当前目录下的 test.txt 文件
-att1 = MIMEText(open('/2017-04-24.zip', 'rb').read(), 'base64', 'utf-8')
-att1["Content-Type"] = 'application/octet-stream'
-# 这里的filename可以任意写，写什么名字，邮件中显示什么名字
-att1["Content-Disposition"] = 'attachment; filename="2017-04-24.zip"'
-message.attach(att1)
+	# # 构造附件2，传送当前目录下的 runoob.txt 文件
+	# att2 = MIMEText(open('runoob.txt', 'rb').read(), 'base64', 'utf-8')
+	# att2["Content-Type"] = 'application/octet-stream'
+	# att2["Content-Disposition"] = 'attachment; filename="runoob.txt"'
+	# message.attach(att2)
 
-# # 构造附件2，传送当前目录下的 runoob.txt 文件
-# att2 = MIMEText(open('runoob.txt', 'rb').read(), 'base64', 'utf-8')
-# att2["Content-Type"] = 'application/octet-stream'
-# att2["Content-Disposition"] = 'attachment; filename="runoob.txt"'
-# message.attach(att2)
+	try:
+	    server = smtplib.SMTP("smtp.aliyun.com", 25)  # 发件人邮箱中的SMTP服务器，端口是25
+	    server.login(my_sender, "wsx19961021")
 
-try:
-    server = smtplib.SMTP("smtp.aliyun.com", 25)  # 发件人邮箱中的SMTP服务器，端口是25
-    server.login(my_sender, "wsx19961021")
-
-    server.sendmail(my_sender, my_user, message.as_string())
-    print("邮件发送成功")
-except smtplib.SMTPException:
-    print("Error: 无法发送邮件")
+	    server.sendmail(my_sender, my_user, message.as_string())
+	    print("邮件发送成功")
+	except smtplib.SMTPException:
+	    print("Error: 无法发送邮件")
