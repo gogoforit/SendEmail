fce8a5fea36bb28ac5a9c79b1c03d8cc67e5c1af
gogoforit <715157026@qq.com>
2017-04-30 15:48:27 +0800

    同时可以向两个邮箱发送邮件

--- SendTest_File.py
+++ SendTest_File.py
@@ -9,6 +9,7 @@ from email.header import Header
 def send_email(file_path,file_name,the_time):
 	my_sender='wangxinyuan_sign@aliyun.com' #发件人邮箱账号，为了后面易于维护，所以写成了变量
 	my_user='715157026@qq.com' #收件人邮箱账号，为了后面易于维护，所以写成了变量
+	my_user2 = 'wangxinyuan_info@aliyun.com'
 	#file_path = ''
 	#file_name = ''
 	title_name = '每日签到文件' + the_time 
@@ -45,6 +46,9 @@ def send_email(file_path,file_name,the_time):
 	    server.login(my_sender, "wsx19961021")
 
 	    server.sendmail(my_sender, my_user, message.as_string())
-	    print("邮件发送成功")
+	    print("1邮件发送成功")
+	    server.sendmail(my_sender, my_user2, message.as_string())
+	    print("2邮件发送成功")
+
 	except smtplib.SMTPException:
 	    print("Error: 无法发送邮件")
